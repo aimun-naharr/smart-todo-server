6af93dc2095b666798fb8d3bb014f24bfd047016
aimunNaharr <aimunnaharr@gmail.com>
2023-04-02 21:48:05 +0600

    app file created

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+import cors from 'cors'
+
+const app=express()
+
+// middlewares
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+  methods: ['GET','PUT','DELETE', 'POST', 'PATCH'],
+  allowedHeaders: 'Content-Type,Authorization'
+}
+app.use(cors(corsOptions));
+app.use(bodyParser.json({limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
+
+app.get("/", (req, res) => {
+   res.send("Route is working! YaY!");
+   res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
+ 
+ 
+export default app
